353b33bfda718cafc4147794371ebf270f456205
ekoeryanto <ekoeryanto@gmail.com>
2022-05-15 11:33:20 +0700

    fix: wrap plugins with fastify-plugin

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,13 +1,14 @@
-import { FastifyPluginAsync } from 'fastify';
+import fp from 'fastify-plugin';
 import sensible from '@fastify/sensible';
+
 import support from './support';
 
-export interface PluginOptions {}
+export interface PluginOptions { }
 
-export const plugins: FastifyPluginAsync<PluginOptions> = async (app) => {
+export const plugins = fp(async (app) => {
   // register plugins
   app.register(sensible);
 
   // register custom plugins
   app.register(support);
-};
+});
